e87a71689790e724a7b7170f1e721e8b90cb3140
lukuiyuan <lukuiyuan@bytedance.com>
2020-10-24 17:37:56 +0800

    feat: 验证部署上nginx后js文件能否正常加载

--- famp_fe/src/pages/HomePage/index.jsx
+++ famp_fe/src/pages/HomePage/index.jsx
@@ -8,12 +8,17 @@ class HomePage extends Component {
     }
   }
 
+  gotoBaidu = () => {
+    window.location.href = 'https://www.baidu.com';
+  }
+
   render() {
     
     return (   
         <div>
             <div className="note">欢迎进入FA监控平台</div>
             <div className="note">不过，仍在开发中。。。现在啥也还没</div>
+            <div className="note" onClick={this.gotoBaidu}>百度以下，你就知道</div>
         </div>
     );
   }
